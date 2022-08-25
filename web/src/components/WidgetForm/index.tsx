6f99ffe767b05e0f9207ee95ed1ca37ba0ab66ee
fiorotticaio <caiofiorotti@gmail.com>
2022-08-25 11:44:02 -0300

    front-end done

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -7,6 +7,7 @@ import ideaImageUrl from '../../assets/idea.svg';
 import thoughtImageUrl from '../../assets/thought.svg';
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
 
 export const feedbackTypes = {
     BUG: {
@@ -46,22 +47,37 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 
 export function WidgetForm() {
-
     const [feedbackType, setFeedBackType] = useState<FeedbackType | null>(null);
+    const [feedbackSent, setFeedbackSent] = useState(false);
 
+    function handleRestartFeedback() {
+        setFeedbackSent(false);
+        setFeedBackType(null);
+    }
 
 
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-
-            {!feedbackType ? (
-                <FeedbackTypeStep onFeedbackTypeChanged={setFeedBackType} />
+            
+            { feedbackSent ? (
+                <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback} />
             ) : (
-                <FeedbackContentStep />
-            )}
+                <>
+                    {!feedbackType ? (
+                        <FeedbackTypeStep onFeedbackTypeChanged={setFeedBackType} />
+                    ) : (
+                        <FeedbackContentStep 
+                            feedbackType={feedbackType}
+                            onFeedbackRestartRequested={handleRestartFeedback}
+                            onFeedbackSent={() => setFeedbackSent(true)}
+                        />
+                    )}
+                </>
+            ) }
+            
 
             <footer className="text-xs text-neutral-400">
-                Feito com ♥ pela <a className="underline underline-offset-2" href="https://www.rocketseat.com.br/">Rocketseat</a> 
+                Feito com ♥ por <a className="underline underline-offset-2" href="https://github.com/fiorotticaio">Caio Fiorotti</a> 
             </footer>
         </div>
     )
